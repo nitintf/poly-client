df476d6988b250421ed91b09d348a30a3aef9e95
Nitin Panwar <nitin.panwar@bitcs.in>
2023-08-28 02:40:54 +0530

    feat: authentication flow

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
-import { useState } from 'react';
 import { config } from 'config';
-import reactLogo from './assets/react.svg';
 import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
   const apiUrl = config.app.origin;
 
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((c) => c + 1)}>
-          count is {count} {apiUrl}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="card">
+      <p>{apiUrl}</p>
+    </div>
   );
 }
 
